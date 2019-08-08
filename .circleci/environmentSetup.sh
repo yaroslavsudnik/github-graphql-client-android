fc207d3a92a7086374d3739d679c04b579976bfb
Yaroslav Sudnik <yaroslav.sudnik@flatstack.com>
2019-08-08 19:33:41 +0300

    add github token to ci environment

---
+++ .circleci/environmentSetup.sh
@@ -0,0 +1,15 @@
+function copyEnvVarsToGradleProperties {
+    GRADLE_PROPERTIES=$HOME"/.gradle/gradle.properties"
+    export GRADLE_PROPERTIES
+    echo "Gradle Properties should exist at $GRADLE_PROPERTIES"
+
+    if [ ! -f "$GRADLE_PROPERTIES" ]; then
+        echo "Gradle Properties does not exist"
+
+        echo "Creating Gradle Properties file..."
+        touch $GRADLE_PROPERTIES
+
+        echo "Writing github_token to gradle.properties..."
+        echo "github_token=$github_token" >> $GRADLE_PROPERTIES
+    fi
+}
